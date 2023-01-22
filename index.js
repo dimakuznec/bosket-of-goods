aac10fed08003b80877470b12db3abf5fa7ab3e1
Dima Kuznecov <kud3544@gmail.com>
2023-01-22 13:16:51 +0300

    git commit

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-//const Tshirt2Node = document.querySelector('.Tshirt2');
-//const inputNode = document.querySelector('.input');
-//const buttonTshirt2 = document.querySelector('.buttonTshirt2')
+const functionalityProducts = document.querySelector('.js-functionality-products');
+const jsButton = document.querySelector('.js-button');
+const jsAlertAlertSecondary = document.querySelector('.js-alert-alert-secondary');
+const jsCardBodyBorderTop = document.querySelector('.js-card-body-border-top');
 
+functionalityProducts.addEventListener('click', function() {
+    const purchase = jsAlertAlertSecondary.value;
+    let output = 'Товар в карзине';
+    
+    if (purchase == 'functionalityProducts') {
+        output ='Ашбка';
+    }
+    
+    jsAlertAlertSecondary.innerHTML = output;
+    });
+    
