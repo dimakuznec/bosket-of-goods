30d7873c3389dc24edb35c0a06ae4d58c1405dcc
Dima Kuznecov <kud3544@gmail.com>
2023-02-10 17:14:24 +0300

    git commit

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-const functionalityProducts = document.querySelector('.js-functionality-products');
+// Все объявленные переменные
 const jsAlertAlertSecondary = document.querySelector('.js-alert-alert-secondary');
 
+//Массив с товарами
+let products = [
+  bread = document.querySelector('.bread'),
+  Ioaf = document.querySelector('.Ioaf'),
+  croisant = document.querySelector('.croisant'),
+  cupcakes = document.querySelector('.cupcakes'),
+  donut = document.querySelector('.donut'),
+  buns = document.querySelector('.buns'),
+];
+
 // Вывод товара в корзину
-functionalityProducts.addEventListener('click', function() {
-    const purchase = jsAlertAlertSecondary.value;
-    let output = 'Товар в карзине';
-    jsAlertAlertSecondary.innerHTML = output; 
+bread.addEventListener('click', function() {
+  const purchase = jsAlertAlertSecondary.value;
+  let output = 'Хлеб';
+  jsAlertAlertSecondary.innerHTML = output; 
+});
+
+Ioaf.addEventListener('click', function() {
+  const purchase = jsAlertAlertSecondary.value;
+  let output = 'Батоны';
+  jsAlertAlertSecondary.innerHTML = output; 
+});
+
+croisant.addEventListener('click', function() {
+  const purchase = jsAlertAlertSecondary.value;
+  let output = 'Круасаны';
+  jsAlertAlertSecondary.innerHTML = output; 
+});
+
+cupcakes.addEventListener('click', function() {
+  const purchase = jsAlertAlertSecondary.value;
+  let output = 'Кексы';
+  jsAlertAlertSecondary.innerHTML = output; 
+});
+
+donut.addEventListener('click', function() {
+  const purchase = jsAlertAlertSecondary.value;
+  let output = 'Пончики';
+  jsAlertAlertSecondary.innerHTML = output; 
+});
+
+buns.addEventListener('click', function() {
+  const purchase = jsAlertAlertSecondary.value;
+  let output = 'Булочки';
+  jsAlertAlertSecondary.innerHTML = output; 
 });
 
 //Функция удаления товара из карзины
